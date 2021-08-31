f8ba2f896a2868e0a255a3fceddcf1e9d7578a83
ghani <ghani.rizkymail@gmail.com>
2021-08-31 13:41:18 +0700

    test: testing

--- src/helpers/readFile.js
+++ src/helpers/readFile.js
@@ -6,6 +6,7 @@ exports.readFile = async(file_name) => {
 
     let rawdata = await fs.readFileSync(`${PATH_FILE}/${file_name}`);
     let data = JSON.parse(rawdata);
+    console.log(rawdata, "<< DATA LAST_UPDATE")
 
     return data
 }
