74da4402cf5db6f4faeb88c4b91a13a0a2a368f9
ghani <ghani.rizkymail@gmail.com>
2021-08-31 22:23:44 +0700

    update : setting channel to send message

--- src/bot.js
+++ src/bot.js
@@ -1,26 +1,23 @@
 require('dotenv').config();
 const constant = require('./common/constant')
 const {game3rb, freegames} = require('./controller')
-
-
 const { Client, Intents } = require('discord.js');
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 
 client.on('ready', async () => {
     console.log(`${client.user.tag} has logged in.`)
-    const channel = client.channels.cache.get('880697006224470019');
+    const channelGame3rb = client.channels.cache.get('881639659577425950');
+    const channelFreeGames = client.channels.cache.get('882283760215818251');
 
-    await game3rb.createMessage(channel)
-    await freegames.createMessage(channel)
+    await game3rb.createMessage(channelGame3rb)
+    await freegames.createMessage(channelFreeGames)
     setInterval(async() => {
-        await game3rb.createMessage(channel)
+        await game3rb.createMessage(channelGame3rb)
     }, 600000)
 
     setInterval(async() => {
-        await freegames.createMessage(channel)
+        await freegames.createMessage(channelFreeGames)
     }, 86400000)
-
 })
 
 client.login(constant.DISCORD_BOT_TOKEN)
